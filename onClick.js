08412a095bea317590e49c6d32bd8785a728eea7
Andre <jcsteele26@gmail.com>
2020-04-15 14:59:44 -0400

    exercises

---
+++ onClick.js
@@ -0,0 +1,9 @@
+<script>
+    document.addEventListener('DOMContentLoaded', function(){
+      // Add your code below this line
+  document.getElementById('getMessage').onclick = function(){};
+  
+  
+      // Add your code above this line
+    });
+  </script>
